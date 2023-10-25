aa51841cc7376b1e7aacac5c7d28f8b21f2755b1
dave8git <dave8git@gmail.com>
2023-10-26 00:54:33 +0200

    Add redux

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,62 +1,11 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
 import ColumnForm from '../ColumnForm/ColumnForm';
-import { useState, useEffect } from 'react';
-import shortid from 'shortid';
+import { useSelector } from 'react-redux';
+
 
 const List = () => {
-    const [columns, setColumns] = useState([
-        {
-            id: 1,
-            title: 'Books',
-            icon: 'book',
-            cards: [
-                { id: 1, title: 'This is Going to Hurt' },
-                { id: 2, title: 'Interpreter of Maladies' }
-            ]
-        },
-        {
-            id: 2,
-            title: 'Movies',
-            icon: 'film',
-            cards: [
-                { id: 1, title: 'Harry Potter' },
-                { id: 2, title: 'Star Wars' }
-            ]
-        },
-        {
-            id: 3,
-            title: 'Games',
-            icon: 'gamepad',
-            cards: [
-                { id: 1, title: 'The Witcher' },
-                { id: 2, title: 'Skyrim' }
-            ]
-        }
-    ]);
-
-    const [value, setValue] = useState("");
-
-
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setColumns([...columns, {id: 4, title: 'Test column'}])
-    //     }, 2000);
-    // }, []);
-  
-    const addColumn = newColumn => {
-        setColumns([...columns, {id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: []  }]);
-    }
-
-    const addCard = (newCard, columnId) => {
-        const columnsUpdated = columns.map(column => {
-            if(column.id === columnId)
-                return {...column, cards: [...column.cards, { id: shortid(), title: newCard.title } ]}
-            else 
-                return column
-        })
-        setColumns(columnsUpdated);
-    }
+    const columns = useSelector(state => state.columns);
 
     return (
         <div className={styles.list}>
@@ -66,7 +15,7 @@ const List = () => {
             <p className={styles.description}>Interesting things I want to check out</p>
             <section className={styles.columns}>
                 {columns.map((column) => 
-                    <Column key={column.id} id={column.id} action={addCard} title={column.title} icon={column.icon} cards={column.cards}  />
+                    <Column key={column.id} id={column.id} {...column} />
                 )}
                 {/* <Column title="Books" icon="book" />
                 <Column title="Movies" icon="film" />
@@ -81,7 +30,7 @@ const List = () => {
                     <h2 className="title">Games</h2>
                 </article> */}
             </section>
-            <ColumnForm action={addColumn}/>
+            <ColumnForm />
 
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; 
+import store from './redux/store';
 import App from './App';
 import 'font-awesome/css/font-awesome.min.css';
 import './styles/normalize.scss';
@@ -6,6 +9,10 @@ import './styles/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />, // app to też jest funkcja
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />, // app to też jest funkcja
+    </Provider>
+  </React.StrictMode>,
 );
 
